68beab23349a3c5150d0fc23453aa0c3b64b5960
RobertoFORTs <bobfortesneto@gmail.com>
2023-02-09 18:04:57 -0300

    feat: add checkUserRegistration and checkUserLogin

--- FirstChallenge/src/controllers/userControllers.js
+++ FirstChallenge/src/controllers/userControllers.js
@@ -4,6 +4,37 @@ const fs = require('fs');
 
 const backup = JSON.parse(fs.readFileSync(`${__dirname}/./../data/user.json`));
 
+checkEmail = (req,res) =>{
+
+}
+
+checkPassword = (req,res) =>{
+
+}
+
+exports.checkUserRegistration = (req,res,next) => {
+
+    if (!req.body.firstName||!req.body.lastName||!req.body.birthDate||!req.body.city||!req.body.country||!req.body.email||!req.body.password||!req.body.confirmPassword){ //maybe add a function to check types later
+        
+        return res.status(400).json({
+            status: 'failed',
+            message: 'missing required information'
+            });
+    }
+    next();
+
+};
+
+exports.checkUserLogin = (req,res,next) => {
+    if (!req.body.email||!req.body.password){
+        return res.status(400).json({
+        status: 'failed',
+        message: 'missing required information'
+        });
+    }
+    next();
+}
+
 
 exports.signUserUp = (req,res) => {
     // if (backup.find(el => el.email === req.body.email)){ //user already exists
